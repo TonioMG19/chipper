b1457c1bb80dc2977013c4a59a984820dadb3229
TonioMG19 <62599905+TonioMG19@users.noreply.github.com>
2020-12-12 16:16:34 +0100

    first commit

---
+++ PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : PhysicsObject
+{
+    // ici en public ce sont les differentes variables que l'on peut modifier dans l'inspector 
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // la fonction en dessous permet le déplacement de votre personnage
+    protected override void ComputeVelocity()
+    {
+
+        float a = transform.position.x;
+
+
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0) : (move.x < 0));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+    }
+
+    // Ajouter ici vos fonctions 
+
+    void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.tag == "coin")
+        {
+            col.gameObject.SetActive(false);
+        }
+    }
+
+}
